2e90e38965fc5b4d2ac2938cf258e3a5be41ee88
Atif Aziz <code@raboof.com>
2011-12-07 21:15:40 +0100

    Data selectors for DataTable

--- src/DataSelectors.g.cs
+++ src/DataSelectors.g.cs
@@ -32,7 +32,7 @@ namespace Eggado
     #endregion
 
     // This partial implementation was template-generated:
-    // Wed, 07 Dec 2011 19:24:00 GMT
+    // Wed, 07 Dec 2011 20:11:49 GMT
 
     partial class DataReaderExtensions
     { 
@@ -367,6 +367,159 @@ namespace Eggado
             if (selector == null) throw new ArgumentNullException("selector");
 
             return Eggnumerable.From(command.ExecuteReader, r => r.Select(selector));
+        }
+    }
+
+    partial class DataTableExtensions
+    { 
+        public static IEnumerable<TResult> Select<T1, T2, TResult>(
+            this DataTable table,
+            Func<T1, T2, TResult> selector)
+        {
+            if (table == null) throw new ArgumentNullException("table");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+        }
+
+        public static IEnumerable<TResult> Select<T1, T2, T3, TResult>(
+            this DataTable table,
+            Func<T1, T2, T3, TResult> selector)
+        {
+            if (table == null) throw new ArgumentNullException("table");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+        }
+
+        public static IEnumerable<TResult> Select<T1, T2, T3, T4, TResult>(
+            this DataTable table,
+            Func<T1, T2, T3, T4, TResult> selector)
+        {
+            if (table == null) throw new ArgumentNullException("table");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+        }
+
+        public static IEnumerable<TResult> Select<T1, T2, T3, T4, T5, TResult>(
+            this DataTable table,
+            Func<T1, T2, T3, T4, T5, TResult> selector)
+        {
+            if (table == null) throw new ArgumentNullException("table");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+        }
+
+        public static IEnumerable<TResult> Select<T1, T2, T3, T4, T5, T6, TResult>(
+            this DataTable table,
+            Func<T1, T2, T3, T4, T5, T6, TResult> selector)
+        {
+            if (table == null) throw new ArgumentNullException("table");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+        }
+
+        public static IEnumerable<TResult> Select<T1, T2, T3, T4, T5, T6, T7, TResult>(
+            this DataTable table,
+            Func<T1, T2, T3, T4, T5, T6, T7, TResult> selector)
+        {
+            if (table == null) throw new ArgumentNullException("table");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+        }
+
+        public static IEnumerable<TResult> Select<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(
+            this DataTable table,
+            Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult> selector)
+        {
+            if (table == null) throw new ArgumentNullException("table");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+        }
+
+        public static IEnumerable<TResult> Select<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult>(
+            this DataTable table,
+            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult> selector)
+        {
+            if (table == null) throw new ArgumentNullException("table");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+        }
+
+        public static IEnumerable<TResult> Select<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>(
+            this DataTable table,
+            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult> selector)
+        {
+            if (table == null) throw new ArgumentNullException("table");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+        }
+
+        public static IEnumerable<TResult> Select<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult>(
+            this DataTable table,
+            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult> selector)
+        {
+            if (table == null) throw new ArgumentNullException("table");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+        }
+
+        public static IEnumerable<TResult> Select<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult>(
+            this DataTable table,
+            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult> selector)
+        {
+            if (table == null) throw new ArgumentNullException("table");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+        }
+
+        public static IEnumerable<TResult> Select<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult>(
+            this DataTable table,
+            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult> selector)
+        {
+            if (table == null) throw new ArgumentNullException("table");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+        }
+
+        public static IEnumerable<TResult> Select<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult>(
+            this DataTable table,
+            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult> selector)
+        {
+            if (table == null) throw new ArgumentNullException("table");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+        }
+
+        public static IEnumerable<TResult> Select<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult>(
+            this DataTable table,
+            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult> selector)
+        {
+            if (table == null) throw new ArgumentNullException("table");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+        }
+
+        public static IEnumerable<TResult> Select<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult>(
+            this DataTable table,
+            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult> selector)
+        {
+            if (table == null) throw new ArgumentNullException("table");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
         }
     }
 }
---
+++ src/DataTableExtensions.cs
@@ -0,0 +1,60 @@
+#region License, Terms and Author(s)
+//
+// Eggado
+// Copyright (c) 2011 Atif Aziz. All rights reserved.
+//
+//  Author(s):
+//
+//      Atif Aziz, http://www.raboof.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
+namespace Eggado
+{
+    #region Imports
+
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using JetBrains.Annotations;
+
+    #endregion
+
+    public static partial class DataTableExtensions
+    {
+        public static IEnumerable<T> Select<T>([NotNull] this DataTable table) 
+            where T : new()
+        {
+            if (table == null) throw new ArgumentNullException("table");
+            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select<T>());
+        }
+
+        public static IEnumerable<TResult> Select<T, TResult>(
+            [NotNull] this DataTable table,
+            [NotNull] Func<T, TResult> selector)
+        {
+            if (table == null) throw new ArgumentNullException("table");
+            if (selector == null) throw new ArgumentNullException("selector");
+            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+        }
+
+        public static IEnumerable<dynamic> Select(
+            [NotNull] this DataTable table)
+        {
+            if (table == null) throw new ArgumentNullException("table");
+            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select());
+        }
+    }
+}
