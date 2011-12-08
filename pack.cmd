bc193b859a3d6cb3c3baa2cf78742b6d8df5004a
Atif Aziz <code@raboof.com>
2011-12-08 08:26:01 +0100

    Targeting full profile in package due to System.Runtime.Caching dependency

--- pack.cmd
+++ pack.cmd
@@ -9,9 +9,10 @@ setlocal
 pushd "%~dp0pkg"
 if exist base rd base /s /q
 if not errorlevel 0 exit /b %errorlevel%
-if not exist base\lib\net40 md base\lib\net40
+set net40=base\lib\net40-full
+if not exist "%net40%" md "%net40%"
 if not errorlevel 0 exit /b %errorlevel%
 copy ..\COPYING*.txt base > nul ^
- && copy ..\bin\Release base\lib\net40 > nul ^
+ && copy ..\bin\Release "%net40%" > nul ^
  && nuget pack Eggado.nuspec -BasePath base
 goto :EOF
