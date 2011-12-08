c6db9e194592251572ec13d988f7d1f96b5b5cf3
Atif Aziz <code@raboof.com>
2011-12-08 09:41:07 +0100

    Removing redundant profile name for packaging

--- pack.cmd
+++ pack.cmd
@@ -9,7 +9,7 @@ setlocal
 pushd "%~dp0pkg"
 if exist base rd base /s /q
 if not errorlevel 0 exit /b %errorlevel%
-set net40=base\lib\net40-full
+set net40=base\lib\net40
 if not exist "%net40%" md "%net40%"
 if not errorlevel 0 exit /b %errorlevel%
 copy ..\COPYING*.txt base > nul ^
