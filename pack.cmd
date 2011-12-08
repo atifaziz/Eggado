d1f94e28672c01c8f543d1631bb5f5cdf40326d3
Atif Aziz <code@raboof.com>
2011-12-08 08:03:37 +0100

    Clean up base in pack batch before each re-packaging

--- pack.cmd
+++ pack.cmd
@@ -7,7 +7,10 @@ goto :EOF
 :pack
 setlocal
 pushd "%~dp0pkg"
+if exist base rd base /s /q
+if not errorlevel 0 exit /b %errorlevel%
 if not exist base\lib\net40 md base\lib\net40
+if not errorlevel 0 exit /b %errorlevel%
 copy ..\COPYING*.txt base > nul ^
  && copy ..\bin\Release base\lib\net40 > nul ^
  && nuget pack Eggado.nuspec -BasePath base
