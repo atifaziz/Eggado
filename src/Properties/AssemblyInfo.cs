7b6d01956211716cf452cca34cdabf46921699cc
Atif Aziz <code@raboof.com>
2011-12-20 08:41:23 +0100

    Setting version to 0.9.14320 plus release notes

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -44,8 +44,8 @@ using ComVisible = System.Runtime.InteropServices.ComVisibleAttribute;
 // Version information
 //
 
-[assembly: AssemblyVersion("0.9.14308.0")]
-[assembly: AssemblyFileVersion("0.9.14308.0831")]
+[assembly: AssemblyVersion("0.9.14320.0")]
+[assembly: AssemblyFileVersion("0.9.14320.0")]
 
 //
 // Configuration (test, debug, release)
