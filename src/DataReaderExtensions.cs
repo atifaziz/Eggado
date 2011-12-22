eb55c86d64834f6993c12d902404bd4a11e28849
Atif Aziz <code@raboof.com>
2011-12-22 21:10:33 +0100

    Dead code in comment

--- src/DataReaderExtensions.cs
+++ src/DataReaderExtensions.cs
@@ -177,10 +177,6 @@ namespace Eggado
              "_1",  "_2",  "_3",  "_4",  "_5",  "_6",  "_7",  "_8",  "_9", "_10", 
             "_11", "_12", "_13", "_14", "_15", "_16", "_17", "_18", "_19", "_20",
         };
-            /*
-            Enumerable.Range(1, 20)
-                      .Select(i => "_" + i.ToString(CultureInfo.InvariantCulture))
-                      .ToArray();*/
 
         private static IEnumerable<Mapping> EnumerateMappings(IDataRecord source, Delegate target)
         {
