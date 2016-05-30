043e07eee06f9a168a68e45fec827651c5c89c2a
Atif Aziz <code@raboof.com>
2016-05-30 09:37:33 +0200

    Terser DataTable.CreateDataReader in generated code

--- src/DataSelectors.g.cs
+++ src/DataSelectors.g.cs
@@ -32,7 +32,7 @@ namespace Eggado
     #endregion
 
     // This partial implementation was template-generated:
-    // Wed, 07 Dec 2011 20:11:49 GMT
+    // Mon, 30 May 2016 07:31:25 GMT
 
     partial class DataReaderExtensions
     { 
@@ -379,7 +379,7 @@ namespace Eggado
             if (table == null) throw new ArgumentNullException("table");
             if (selector == null) throw new ArgumentNullException("selector");
 
-            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+            return Eggnumerable.From(table.CreateDataReader, r => r.Select(selector));
         }
 
         public static IEnumerable<TResult> Select<T1, T2, T3, TResult>(
@@ -389,7 +389,7 @@ namespace Eggado
             if (table == null) throw new ArgumentNullException("table");
             if (selector == null) throw new ArgumentNullException("selector");
 
-            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+            return Eggnumerable.From(table.CreateDataReader, r => r.Select(selector));
         }
 
         public static IEnumerable<TResult> Select<T1, T2, T3, T4, TResult>(
@@ -399,7 +399,7 @@ namespace Eggado
             if (table == null) throw new ArgumentNullException("table");
             if (selector == null) throw new ArgumentNullException("selector");
 
-            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+            return Eggnumerable.From(table.CreateDataReader, r => r.Select(selector));
         }
 
         public static IEnumerable<TResult> Select<T1, T2, T3, T4, T5, TResult>(
@@ -409,7 +409,7 @@ namespace Eggado
             if (table == null) throw new ArgumentNullException("table");
             if (selector == null) throw new ArgumentNullException("selector");
 
-            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+            return Eggnumerable.From(table.CreateDataReader, r => r.Select(selector));
         }
 
         public static IEnumerable<TResult> Select<T1, T2, T3, T4, T5, T6, TResult>(
@@ -419,7 +419,7 @@ namespace Eggado
             if (table == null) throw new ArgumentNullException("table");
             if (selector == null) throw new ArgumentNullException("selector");
 
-            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+            return Eggnumerable.From(table.CreateDataReader, r => r.Select(selector));
         }
 
         public static IEnumerable<TResult> Select<T1, T2, T3, T4, T5, T6, T7, TResult>(
@@ -429,7 +429,7 @@ namespace Eggado
             if (table == null) throw new ArgumentNullException("table");
             if (selector == null) throw new ArgumentNullException("selector");
 
-            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+            return Eggnumerable.From(table.CreateDataReader, r => r.Select(selector));
         }
 
         public static IEnumerable<TResult> Select<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(
@@ -439,7 +439,7 @@ namespace Eggado
             if (table == null) throw new ArgumentNullException("table");
             if (selector == null) throw new ArgumentNullException("selector");
 
-            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+            return Eggnumerable.From(table.CreateDataReader, r => r.Select(selector));
         }
 
         public static IEnumerable<TResult> Select<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult>(
@@ -449,7 +449,7 @@ namespace Eggado
             if (table == null) throw new ArgumentNullException("table");
             if (selector == null) throw new ArgumentNullException("selector");
 
-            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+            return Eggnumerable.From(table.CreateDataReader, r => r.Select(selector));
         }
 
         public static IEnumerable<TResult> Select<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>(
@@ -459,7 +459,7 @@ namespace Eggado
             if (table == null) throw new ArgumentNullException("table");
             if (selector == null) throw new ArgumentNullException("selector");
 
-            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+            return Eggnumerable.From(table.CreateDataReader, r => r.Select(selector));
         }
 
         public static IEnumerable<TResult> Select<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult>(
@@ -469,7 +469,7 @@ namespace Eggado
             if (table == null) throw new ArgumentNullException("table");
             if (selector == null) throw new ArgumentNullException("selector");
 
-            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+            return Eggnumerable.From(table.CreateDataReader, r => r.Select(selector));
         }
 
         public static IEnumerable<TResult> Select<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult>(
@@ -479,7 +479,7 @@ namespace Eggado
             if (table == null) throw new ArgumentNullException("table");
             if (selector == null) throw new ArgumentNullException("selector");
 
-            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+            return Eggnumerable.From(table.CreateDataReader, r => r.Select(selector));
         }
 
         public static IEnumerable<TResult> Select<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult>(
@@ -489,7 +489,7 @@ namespace Eggado
             if (table == null) throw new ArgumentNullException("table");
             if (selector == null) throw new ArgumentNullException("selector");
 
-            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+            return Eggnumerable.From(table.CreateDataReader, r => r.Select(selector));
         }
 
         public static IEnumerable<TResult> Select<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult>(
@@ -499,7 +499,7 @@ namespace Eggado
             if (table == null) throw new ArgumentNullException("table");
             if (selector == null) throw new ArgumentNullException("selector");
 
-            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+            return Eggnumerable.From(table.CreateDataReader, r => r.Select(selector));
         }
 
         public static IEnumerable<TResult> Select<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult>(
@@ -509,7 +509,7 @@ namespace Eggado
             if (table == null) throw new ArgumentNullException("table");
             if (selector == null) throw new ArgumentNullException("selector");
 
-            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+            return Eggnumerable.From(table.CreateDataReader, r => r.Select(selector));
         }
 
         public static IEnumerable<TResult> Select<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult>(
@@ -519,7 +519,7 @@ namespace Eggado
             if (table == null) throw new ArgumentNullException("table");
             if (selector == null) throw new ArgumentNullException("selector");
 
-            return Eggnumerable.From(() => new DataTableReader(table), r => r.Select(selector));
+            return Eggnumerable.From(table.CreateDataReader, r => r.Select(selector));
         }
     }
 }
