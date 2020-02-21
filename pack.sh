9c334e6f4512e8c3b8c63cc050a79a92b56a4863
Atif Aziz <code@raboof.com>
2020-02-21 17:59:52 +0100

    Move packaging properties into project file

--- pack.sh
+++ pack.sh
@@ -7,8 +7,4 @@ else
     VERSION_SUFFIX=
 fi
 ./build.sh
-dotnet pack                                          \
-       --no-build --include-symbols --include-source \
-       -c Release -o ../dist                         \
-       $VERSION_SUFFIX                               \
-       src
+dotnet pack --no-build $VERSION_SUFFIX src
