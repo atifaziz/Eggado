4610ba28de025f4986fc08805ea9fce3491e6ecd
Atif Aziz <code@raboof.com>
2020-02-27 09:43:02 +0100

    Remove cancellation token from SelectAsync for command

--- src/DataSelectors.g.cs
+++ src/DataSelectors.g.cs
@@ -28,7 +28,7 @@ namespace Eggado
     #endregion
 
     // This partial implementation was template-generated:
-    // Tue, 25 Feb 2020 09:48:31 GMT
+    // Thu, 27 Feb 2020 08:41:30 GMT
 
     partial class DataReaderExtensions
     {
@@ -1034,210 +1034,178 @@ namespace Eggado
     {
         public static IAsyncEnumerable<TResult> SelectAsync<T, TResult>(
             this DbCommand command,
-            Func<T, TResult> selector,
-            CancellationToken cancellationToken = default)
+            Func<T, TResult> selector)
         {
             if (command == null) throw new ArgumentNullException(nameof(command));
             if (selector == null) throw new ArgumentNullException(nameof(selector));
 
             return Eggnumerable.FromAsync(ct => command.ExecuteReaderAsync(ct),
-                                          (r, ct) => r.SelectAsync(selector, ct),
-                                          cancellationToken);
+                                          (r, ct) => r.SelectAsync(selector, ct));
         }
 
         public static IAsyncEnumerable<TResult> SelectAsync<T1, T2, TResult>(
             this DbCommand command,
-            Func<T1, T2, TResult> selector,
-            CancellationToken cancellationToken = default)
+            Func<T1, T2, TResult> selector)
         {
             if (command == null) throw new ArgumentNullException(nameof(command));
             if (selector == null) throw new ArgumentNullException(nameof(selector));
 
             return Eggnumerable.FromAsync(ct => command.ExecuteReaderAsync(ct),
-                                          (r, ct) => r.SelectAsync(selector, ct),
-                                          cancellationToken);
+                                          (r, ct) => r.SelectAsync(selector, ct));
         }
 
         public static IAsyncEnumerable<TResult> SelectAsync<T1, T2, T3, TResult>(
             this DbCommand command,
-            Func<T1, T2, T3, TResult> selector,
-            CancellationToken cancellationToken = default)
+            Func<T1, T2, T3, TResult> selector)
         {
             if (command == null) throw new ArgumentNullException(nameof(command));
             if (selector == null) throw new ArgumentNullException(nameof(selector));
 
             return Eggnumerable.FromAsync(ct => command.ExecuteReaderAsync(ct),
-                                          (r, ct) => r.SelectAsync(selector, ct),
-                                          cancellationToken);
+                                          (r, ct) => r.SelectAsync(selector, ct));
         }
 
         public static IAsyncEnumerable<TResult> SelectAsync<T1, T2, T3, T4, TResult>(
             this DbCommand command,
-            Func<T1, T2, T3, T4, TResult> selector,
-            CancellationToken cancellationToken = default)
+            Func<T1, T2, T3, T4, TResult> selector)
         {
             if (command == null) throw new ArgumentNullException(nameof(command));
             if (selector == null) throw new ArgumentNullException(nameof(selector));
 
             return Eggnumerable.FromAsync(ct => command.ExecuteReaderAsync(ct),
-                                          (r, ct) => r.SelectAsync(selector, ct),
-                                          cancellationToken);
+                                          (r, ct) => r.SelectAsync(selector, ct));
         }
 
         public static IAsyncEnumerable<TResult> SelectAsync<T1, T2, T3, T4, T5, TResult>(
             this DbCommand command,
-            Func<T1, T2, T3, T4, T5, TResult> selector,
-            CancellationToken cancellationToken = default)
+            Func<T1, T2, T3, T4, T5, TResult> selector)
         {
             if (command == null) throw new ArgumentNullException(nameof(command));
             if (selector == null) throw new ArgumentNullException(nameof(selector));
 
             return Eggnumerable.FromAsync(ct => command.ExecuteReaderAsync(ct),
-                                          (r, ct) => r.SelectAsync(selector, ct),
-                                          cancellationToken);
+                                          (r, ct) => r.SelectAsync(selector, ct));
         }
 
         public static IAsyncEnumerable<TResult> SelectAsync<T1, T2, T3, T4, T5, T6, TResult>(
             this DbCommand command,
-            Func<T1, T2, T3, T4, T5, T6, TResult> selector,
-            CancellationToken cancellationToken = default)
+            Func<T1, T2, T3, T4, T5, T6, TResult> selector)
         {
             if (command == null) throw new ArgumentNullException(nameof(command));
             if (selector == null) throw new ArgumentNullException(nameof(selector));
 
             return Eggnumerable.FromAsync(ct => command.ExecuteReaderAsync(ct),
-                                          (r, ct) => r.SelectAsync(selector, ct),
-                                          cancellationToken);
+                                          (r, ct) => r.SelectAsync(selector, ct));
         }
 
         public static IAsyncEnumerable<TResult> SelectAsync<T1, T2, T3, T4, T5, T6, T7, TResult>(
             this DbCommand command,
-            Func<T1, T2, T3, T4, T5, T6, T7, TResult> selector,
-            CancellationToken cancellationToken = default)
+            Func<T1, T2, T3, T4, T5, T6, T7, TResult> selector)
         {
             if (command == null) throw new ArgumentNullException(nameof(command));
             if (selector == null) throw new ArgumentNullException(nameof(selector));
 
             return Eggnumerable.FromAsync(ct => command.ExecuteReaderAsync(ct),
-                                          (r, ct) => r.SelectAsync(selector, ct),
-                                          cancellationToken);
+                                          (r, ct) => r.SelectAsync(selector, ct));
         }
 
         public static IAsyncEnumerable<TResult> SelectAsync<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(
             this DbCommand command,
-            Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult> selector,
-            CancellationToken cancellationToken = default)
+            Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult> selector)
         {
             if (command == null) throw new ArgumentNullException(nameof(command));
             if (selector == null) throw new ArgumentNullException(nameof(selector));
 
             return Eggnumerable.FromAsync(ct => command.ExecuteReaderAsync(ct),
-                                          (r, ct) => r.SelectAsync(selector, ct),
-                                          cancellationToken);
+                                          (r, ct) => r.SelectAsync(selector, ct));
         }
 
         public static IAsyncEnumerable<TResult> SelectAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult>(
             this DbCommand command,
-            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult> selector,
-            CancellationToken cancellationToken = default)
+            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult> selector)
         {
             if (command == null) throw new ArgumentNullException(nameof(command));
             if (selector == null) throw new ArgumentNullException(nameof(selector));
 
             return Eggnumerable.FromAsync(ct => command.ExecuteReaderAsync(ct),
-                                          (r, ct) => r.SelectAsync(selector, ct),
-                                          cancellationToken);
+                                          (r, ct) => r.SelectAsync(selector, ct));
         }
 
         public static IAsyncEnumerable<TResult> SelectAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>(
             this DbCommand command,
-            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult> selector,
-            CancellationToken cancellationToken = default)
+            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult> selector)
         {
             if (command == null) throw new ArgumentNullException(nameof(command));
             if (selector == null) throw new ArgumentNullException(nameof(selector));
 
             return Eggnumerable.FromAsync(ct => command.ExecuteReaderAsync(ct),
-                                          (r, ct) => r.SelectAsync(selector, ct),
-                                          cancellationToken);
+                                          (r, ct) => r.SelectAsync(selector, ct));
         }
 
         public static IAsyncEnumerable<TResult> SelectAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult>(
             this DbCommand command,
-            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult> selector,
-            CancellationToken cancellationToken = default)
+            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult> selector)
         {
             if (command == null) throw new ArgumentNullException(nameof(command));
             if (selector == null) throw new ArgumentNullException(nameof(selector));
 
             return Eggnumerable.FromAsync(ct => command.ExecuteReaderAsync(ct),
-                                          (r, ct) => r.SelectAsync(selector, ct),
-                                          cancellationToken);
+                                          (r, ct) => r.SelectAsync(selector, ct));
         }
 
         public static IAsyncEnumerable<TResult> SelectAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult>(
             this DbCommand command,
-            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult> selector,
-            CancellationToken cancellationToken = default)
+            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult> selector)
         {
             if (command == null) throw new ArgumentNullException(nameof(command));
             if (selector == null) throw new ArgumentNullException(nameof(selector));
 
             return Eggnumerable.FromAsync(ct => command.ExecuteReaderAsync(ct),
-                                          (r, ct) => r.SelectAsync(selector, ct),
-                                          cancellationToken);
+                                          (r, ct) => r.SelectAsync(selector, ct));
         }
 
         public static IAsyncEnumerable<TResult> SelectAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult>(
             this DbCommand command,
-            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult> selector,
-            CancellationToken cancellationToken = default)
+            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult> selector)
         {
             if (command == null) throw new ArgumentNullException(nameof(command));
             if (selector == null) throw new ArgumentNullException(nameof(selector));
 
             return Eggnumerable.FromAsync(ct => command.ExecuteReaderAsync(ct),
-                                          (r, ct) => r.SelectAsync(selector, ct),
-                                          cancellationToken);
+                                          (r, ct) => r.SelectAsync(selector, ct));
         }
 
         public static IAsyncEnumerable<TResult> SelectAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult>(
             this DbCommand command,
-            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult> selector,
-            CancellationToken cancellationToken = default)
+            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult> selector)
         {
             if (command == null) throw new ArgumentNullException(nameof(command));
             if (selector == null) throw new ArgumentNullException(nameof(selector));
 
             return Eggnumerable.FromAsync(ct => command.ExecuteReaderAsync(ct),
-                                          (r, ct) => r.SelectAsync(selector, ct),
-                                          cancellationToken);
+                                          (r, ct) => r.SelectAsync(selector, ct));
         }
 
         public static IAsyncEnumerable<TResult> SelectAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult>(
             this DbCommand command,
-            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult> selector,
-            CancellationToken cancellationToken = default)
+            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult> selector)
         {
             if (command == null) throw new ArgumentNullException(nameof(command));
             if (selector == null) throw new ArgumentNullException(nameof(selector));
 
             return Eggnumerable.FromAsync(ct => command.ExecuteReaderAsync(ct),
-                                          (r, ct) => r.SelectAsync(selector, ct),
-                                          cancellationToken);
+                                          (r, ct) => r.SelectAsync(selector, ct));
         }
 
         public static IAsyncEnumerable<TResult> SelectAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult>(
             this DbCommand command,
-            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult> selector,
-            CancellationToken cancellationToken = default)
+            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult> selector)
         {
             if (command == null) throw new ArgumentNullException(nameof(command));
             if (selector == null) throw new ArgumentNullException(nameof(selector));
 
             return Eggnumerable.FromAsync(ct => command.ExecuteReaderAsync(ct),
-                                          (r, ct) => r.SelectAsync(selector, ct),
-                                          cancellationToken);
+                                          (r, ct) => r.SelectAsync(selector, ct));
         }
     }
 
